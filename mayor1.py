4e186d243d376a8d6c3c65d5d0f479fabff0dacd
Juan Sebastian Gelvez Carry <000388954@bga.upb>
2019-03-01 11:37:36 -0500

    Creación segundo algoritmo

--- mayor1.py
+++
@@ -1,5 +0,0 @@
-n=int(input("Digite un numero entero"))
-if n>0:
-    print(n,"Es mayor a cero")
-else:
-    print(n,"Es menor a cero")
---
+++ positivos.py
@@ -0,0 +1,5 @@
+a=int(input("Digite un numero"))
+if a%2==0:
+    print(a,"Es un numero par")
+else:
+    print(a,"Es un numero impar")
